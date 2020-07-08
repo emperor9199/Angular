90835d17f9dc9c1863ee0564a61a0dd080d5057a
emperor9199 <199emperor@gmail.com>
2020-07-08 10:05:22 +0530

    create todoList

--- youtube-ang/src/app/app.component.ts
+++ youtube-ang/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'youtube-ang';
+  title = 'TODO';
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.todoValue = '';
+    this.list = [];
+  }
+
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+
+      this.list.push(newItem);
+    }
+
+    this.todoValue = '';
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
